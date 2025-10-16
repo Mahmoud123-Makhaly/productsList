a57e1192fa217540507326901b07986929078617
Mahmoud123-Makhaly <mahmoud.abdelfatah@5d-agency.com>
2025-10-16 17:53:18 +0300

    initialize files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ProductCard from "./components/product-card/ProductCard";
+
+const App = () => {
+  return (
+    <div>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  );
+};
+export default App;
